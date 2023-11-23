1d32698674df113f1b368cf0f4bc53c619e47eb8
Levy Oliveira <103868542+levy-oliveira@users.noreply.github.com>
2023-11-23 14:37:54 -0300

    Adição da função de Cramer e modificação da função de Gauss

--- main.cpp
+++ main.cpp
@@ -1,93 +1,79 @@
 #include <iostream>
-
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
-
-#include <iostream>
 #include <vector>
-
+#include <string>
 using namespace std;
 
-/*ver como acessa posição na linha 21*/
-vector<double> Substituicoes_Retroativas(int n, double **c, vector<double> v){
-    vector<double> d(n);
-    double **C=c;
-    d[n - 1] = v[n - 1] / C[n - 1][n - 1];
-    for (int i = n - 2; i >= 0; i--){
-        double soma = 0;
-        for(int j = i+1; j <= n; j++){
-            soma = soma + C[i][j] * d[j];
-        }
-        d[i] = (v[i] - soma)/C[i][i];
+
+double Det_Elim_Gauss(int n, double c[3][3]){
+  for (int k = 0; k < n; k++){
+    for (int i = k+1; i < n; i++){
+      double m = -(c[i][k])/(c[k][k]);
+      c[i][k] = 0;
+      for (int j = k+1; j < n; j++){
+          c[i][j] = c[i][j] + m * c[k][j];
+      }
     }
-    return d;
+  }
+  double det = 1;
+  for (int l = 0; l < n; l++){
+    det = det * c[l][l];
+  }
+  return det;
 }
 
 
-/*analisar se eh k+1 ou so k*/
-vector<double> Eliminacao_Gauss(int n, vector<vector<double>> **c, vector<double> v){
-    vector<vector<double>> **C = c;
-    for (int k = 0; k < n; k++){
-        for (int i = k+1; i <= n; i++){
-            double m = -(C[i][k])/(C[k][k]);
-            C[i][k] = 0;
-            for (int j = k+1; j <= n; j++){
-                C[i][j] = C[i][j] + m * C[k][j];
-            }
-            v[i] = v[i] + m * v[k];
-        }
+
+double Cramer(int n, double c[3][3], double v[], int i) {
+  double k[3][3];
+  for (int p = 0; p < n; p++) {
+    for (int h = 0; h < n; h++) {
+      k[p][h] = c[p][h];
     }
-    return Substituicoes_Retroativas(n, *c, v);
+  }
+  for (int j = 0; j < n; j++){
+    k[j][i]= v[j];
+  }
+  double det_A = Det_Elim_Gauss(n, c);
+  //cout << "det A" << i + 1 << " = " << det_A << endl;
+  double det_Ax = Det_Elim_Gauss(n, k);
+  //cout << "det Ax" << i + 1 << " = " << det_Ax << endl;
+  double dx = det_Ax / det_A;
+  //cout << "valor" << i + 1 << " = " << dx << endl;
+  return dx;
 }
 
+void imprimirMatriz(double matriz[][3], int linhas, int colunas) {
+  for (int i = 0; i < linhas; ++i) {
+    cout <<"|";  // Nova linha após cada linha da matriz
+    for (int j = 0; j < colunas; ++j) {
+      cout << matriz[i][j] << " ";
+    }
+    cout <<"|"<< endl;  // Nova linha após cada linha da matriz
+  }
+}
 
 int main() {
-    // Exemplo de uso
-    int n = 3;
-    vector<vector<double>> coeficientes = {{2, 1, -1}, {-3, -1, 2}, {-2, 1, 2}};
-    vector<double> termos_independentes = {8, -11, -3};
+  // Exemplo de uso
+  int n = 3;
+  double coeficientes[3][3] = {{2, 1, -1}, {-3, -1, 2}, {-2, 1, 2}};
+  double termos_independentes[3] = {8, -11, -3};
+  double x[3];
 
-    vector<double> solucao = Eliminacao_Gauss(n, *coeficientes, termos_independentes);
-
-    // Exibindo a solução
-    cout << "Solucao:" << endl;
-    for (int i = 0; i < n; i++) {
-        cout << "x" << i + 1 << " = " << solucao[i] << endl;
+  
+  for (int i = 0; i < n; i++) {
+    double a[3][3];
+    for (int p = 0; p < n; p++) {
+      for (int h = 0; h < n; h++) {
+        a[p][h] = coeficientes[p][h];
+      }
     }
+    x[i] = Cramer(n, a,termos_independentes,i);
+  }
+  imprimirMatriz(coeficientes, 3, 3);
+
+  // Exibindo a solução
+  cout << "Solucao:" << endl;
+  for (int i = 0; i < n; i++) {
+      cout << "d" << i + 1 << " = " << x[i] << endl;
+  }
 }
-/*-------------------------------------------------------------------*/
-/*ver como acessa posição na linha 21*/
-vector<double> Substituicoes_Retroativas(int n, vector<std::vector<double>> c, vector<double> v){
-    vector<double> d[n];
-    d[n] = v[n] / (c[n][n]);
-    for(int i = n-1; i >= 1; i--){
-        double soma = 0;
-        for(int j = i+1; j <= n; j++){
-            soma = soma + c[i][j] * d[j];
-        }
-        d[i] = (v[i] - soma)/c[i][i];
-    }
-    return d;
-}
-
-
-/*analisar se eh k+1 ou so k*/
-vector<double> Eliminacao_Gauss(int n, vector<vector<double>> c, vector<double> v){
-    for (int k = 0; k < n; k++){
-        for (int i = k+1; i <= n; i++){
-            double m = -(c[i][k])/(c[k][k]);
-            c[i][k] = 0;
-            for (int j = k+1; j <= n; j++){
-                c[i][j] = c[i][j] + m * c[k][j];
-            }
-            v[i] = v[i] + m * v[k];
-        }
-    }
-    return Substituicoes_Retroativas(n, c, v);
-}
-
-
-
-
